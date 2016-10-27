7a48f6f7bce21aba8557eb3a99e0d3d5975a68ed
arnov-sinha <arnov@udel.edu>
2016-10-27 08:53:12 -0400

    Create forLoopBasic.py

---
+++ forLoopBasic.py
@@ -0,0 +1,28 @@
+def forLoop(n):
+
+    for i in range(0,n): # Its gonna start counting from 1 to n here, 0 is not counted
+        print(str(i+1), ": time in loop")
+
+        #Break example. if you wanted to break from the loop in case a condition
+        #is fulfilled
+        
+        if( i == n-2): 
+            break
+
+
+#For running the code
+forLoop(10)
+
+'''
+Output of the code:
+
+1 : time in loop
+2 : time in loop
+3 : time in loop
+4 : time in loop
+5 : time in loop
+6 : time in loop
+7 : time in loop
+8 : time in loop
+9 : time in loop
+10 : time in loop'''
